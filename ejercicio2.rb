6ec09ae246f5e63f0fb64b9ce4d115e40d2f9000
Eduardo Straub <eduardo.straub1@gmail.com>
2019-01-08 23:14:20 -0300

    Se va probar si funciona.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,5 +1,16 @@
 # rubocop:disable LineLength
 
+# Este modulo es para una Formula.
+module Formula
+  def perimetro(lado1, lado2)
+    2 * lado1 + 2 * lado2
+  end
+
+  def area(lado1, lado2)
+    lado1 * lado2
+  end
+end
+
 # Esta es una clase de un Rectangulo.
 class Rectangulo
   include Formula
@@ -19,19 +30,11 @@ class Cuadrado
   end
 end
 
-# Este modulo es para una Formula.
-module Formula
-  def perimetro(lado1, lado2)
-    2 * lado1 + 2 * lado2
-  end
-
-  def area(lado1, lado2)
-    lado1 * lado2
-  end
-end
-
 rectangulo = Rectangulo.new(4, 2)
-cuadrado = Cuadrado.new(4, 4)
-puts "El area de rectangulo es #{rectangulo.perimetro(rectangulo.lado1, rectangulo.lado2)}"
+cuadrado = Cuadrado.new(4)
+puts "El perimetro de rectangulo es #{rectangulo.perimetro(rectangulo.lado1, rectangulo.lado2)}"
+puts "El area de rectangulo es #{rectangulo.area(rectangulo.lado1, rectangulo.lado2)}"
+puts "El perimetro de cuadrado es #{cuadrado.perimetro(cuadrado.lado, cuadrado.lado)}"
+puts "El area de cuadrado es #{cuadrado.area(cuadrado.lado, cuadrado.lado)}"
 
 # rubocop:enable LineLength
