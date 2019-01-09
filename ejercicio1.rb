035aa43bd0e0e59afcd56401e18754c321743003
Eduardo Straub <eduardo.straub1@gmail.com>
2019-01-08 22:51:13 -0300

    Se completó el ejercicio 1.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -2,10 +2,10 @@
 class Alumno
   def initialize(nombre, nota1, nota2, nota3, nota4)
     @nombre = nombre
-    @nota1 = nota1
-    @nota2 = nota2
-    @nota3 = nota3
-    @nota4 = nota4
+    @nota1 = nota1.to_i
+    @nota2 = nota2.to_i
+    @nota3 = nota3.to_i
+    @nota4 = nota4.to_i
   end
 end
 
@@ -16,8 +16,8 @@ def read_file(text)
   data.each do |alumno|
     alumnos << Alumno.new(*alumno.split(', '))
   end
+  alumnos
 end
 
 alumnos = read_file('notas.txt')
-
-print alumnos
+print "#{alumnos}\n"
